997d51e6f191b7f21aae5ad9ccb7d869baaf626f
Cesde-Suroeste <111468666+Cesde-Suroeste@users.noreply.github.com>
2022-08-18 19:25:47 -0500

    Dejé otro ejemplo más completo para hacer un bucle foreach

--- pull.php
+++ pull.php
@@ -7,3 +7,13 @@ foreach ($persona as $valor)
 { echo "$valor <br>";
 }
 ?>
+
+
+$auto = [ 
+"Marca" => "Ford",
+"Modelo" => "Focus",
+"Año" => 2017
+];
+foreach ($auto as $posicion => $valor) 
+{ echo "$posicion: $valor <br>";
+}
